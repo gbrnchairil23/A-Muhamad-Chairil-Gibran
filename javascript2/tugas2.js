3cea1c18898dcdfeb4bcceb003cdfa67fa87358a
gbrnchairil23 <114551265+gbrnchairil23@users.noreply.github.com>
2024-11-07 20:54:39 +0700

    JS Function

---
+++ javascript2/tugas2.js
@@ -0,0 +1,48 @@
+/*
+function sayHello(person){
+    console.log(`Hi, ${person}`);
+}
+
+sayHello('Letty');
+sayHello('Mazaya')
+*/
+
+// Soal nomor 1
+function printHeart() {
+    console.log("<3");
+}
+printHeart();
+
+
+// Soal nomor 2
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(2, 3));
+console.log(multiply(9, 9));
+console.log(multiply(5, 4));
+
+
+//Soal nomor 3
+let animal = "Giant Pacific Octopus"; 
+function observe(){ 
+    let animal = "Pajama Squid"; 
+    console.log(animal);
+} 
+observe();
+
+let deadlyAnimal = "Blue-Ringed Octopus";
+function handleAnimal() {
+    let deadlyAnimal = "Scorpionfish";
+    console.log(deadlyAnimal);
+}
+handleAnimal();
+console.log(deadlyAnimal)
+
+
+//soal 4
+const sayHello = (name) => `Hello ${name}`;
+
+console.log(sayHello("Hagrid")); 
+console.log(sayHello("Luna")); 
+
